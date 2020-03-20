f139bf2d0cf78ce2037a4d129325ab9ebf82027d
yuseisui <53548640+yuseisui@users.noreply.github.com>
2020-03-21 01:38:00 +0900

    chore: update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   extends: 'standard-with-typescript',
   parserOptions: {
-    project: './tsconfig.json'
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
   }
 }
